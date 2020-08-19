092007bdffacf389209951e1a4a36b79247daa50
juliustao <juliusltao@gmail.com>
2020-08-19 18:02:10 +0000

    fix opencv version to build properly

--- docker/install_dependencies.sh
+++ docker/install_dependencies.sh
@@ -31,7 +31,7 @@ pip install -U setuptools
 apt-get -y install ipython ipython-notebook
 pip install \
   jupyter \
-  opencv-python \
+  opencv-python==4.2.0.32 \
   plyfile \
   pandas \
   tensorflow \
